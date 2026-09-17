fb27def1fbea4d3df261c57bc954ae52e91786ef
Thashwin23 <thashwin1234@gmail.com>
2026-09-17 19:01:58 +0530

    added navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <button className="btn btn-primary">Hello DevMatch</button>
-    </div>
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            {/* <Route path="/" element={<Feed />} /> */}
+            {/* <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} /> */}
+            {/* <Route path="/connections" element={<Connections />} /> */}
+            {/* <Route path="/requests" element={<Requests />} /> */}
+            {/* <Route path="/premium" element={<Premium />} /> */}
+            {/* <Route path="/chat/:targetUserId" element={<Chat />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
