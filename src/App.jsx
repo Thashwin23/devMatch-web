22a3cc5d002065bebba70bc47edfe99fa6fc2d7c
Thashwin23 <thashwin1234@gmail.com>
2026-09-18 21:28:07 +0530

    added connection page and request page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Login from "./components/Login";
 import appStore from "./utils/appStore";
 import Profile from "./components/Profile";
 import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 function App() {
   return (
     <>
@@ -17,8 +19,8 @@ function App() {
               <Route path="/" element={<Feed />} />
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
-              {/* <Route path="/connections" element={<Connections />} /> */}
-              {/* <Route path="/requests" element={<Requests />} /> */}
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
               {/* <Route path="/premium" element={<Premium />} /> */}
               {/* <Route path="/chat/:targetUserId" element={<Chat />} /> */}
             </Route>
