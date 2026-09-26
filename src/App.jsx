a9ca1f77d68b2219be48d3f0a42d7e92fcf74b1e
Thashwin23 <thashwin1234@gmail.com>
2026-09-26 19:40:38 +0530

    added chat ui

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Profile from "./components/Profile";
 import Feed from "./components/Feed";
 import Connections from "./components/Connections";
 import Requests from "./components/Requests";
+import Chat from "./components/chat";
 function App() {
   return (
     <>
@@ -22,7 +23,7 @@ function App() {
               <Route path="/connections" element={<Connections />} />
               <Route path="/requests" element={<Requests />} />
               {/* <Route path="/premium" element={<Premium />} /> */}
-              {/* <Route path="/chat/:targetUserId" element={<Chat />} /> */}
+              <Route path="/chat/:targetUserId" element={<Chat />} />
             </Route>
           </Routes>
         </BrowserRouter>
